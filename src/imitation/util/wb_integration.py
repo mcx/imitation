9dea4e17fc55afcb244c6e14c4811bbea2376768
YawenDuan <35738259+kmdanielduan@users.noreply.github.com>
2021-10-06 03:20:48 -0700

    moving wandb outputformat to seperate file

---
+++ src/imitation/util/wb_integration.py
@@ -0,0 +1,37 @@
+"""W&B integrations"""
+
+import wandb
+
+import stable_baselines3.common.logger as sb_logger
+from typing import Any, Dict, Mapping, Tuple, Union
+
+
+class WandbOutputFormat(sb_logger.KVWriter):
+    """A stable-baseline logger that writes to wandb.
+    """
+    def __init__(self, 
+        wb_options: Mapping[str, Any], 
+        config: Mapping[str, Any],
+    ):
+        """Builds WandbOutputFormat.
+
+        Args:
+            wb_options: A dictionary of options to pass to wandb.init.
+            config: A dictionary of config values to log to wandb.
+
+        """
+        wandb.init(config=config, **wb_options)
+
+    def write(
+        self,
+        key_values: Dict[str, Any],
+        key_excluded: Dict[str, Union[str, Tuple[str, ...]]],
+        step: int = 0,
+    ) -> None:
+        for (key, value), (_, excluded) in zip(sorted(key_values.items()), sorted(key_excluded.items())):
+            if excluded is not None:
+                continue
+            wandb.log({key:value}, step=step)
+
+    def close(self) -> None:
+        wandb.finish()
